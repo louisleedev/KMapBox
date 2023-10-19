3a6d587268a89cca27d1ab92f94a052ba2a7def7
louisleedev <bj.louislee@gmail.com>
2023-10-19 18:37:17 -0400

    Map implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import MapboxMap from "../components/Map";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      <div className="bg-blue-500 text-white p-4">This is a blue box with white text and padding.</div>
-    </main>
-  )
+function App() {
+  return (<div className="w-100% h-1/2">
+    <MapboxMap />
+  </div>)
 }
+
+export default App;
--- src/components/Map.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LocationAggregatorMap = () => {
-    return (
-        <div>Map</div>
-    )
-}
-
-export default LocationAggregatorMap

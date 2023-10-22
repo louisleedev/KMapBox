73e147e28a185c7e1a276fe66a23e89141c9dae4
louisleedev <bj.louislee@gmail.com>
2023-10-21 21:22:04 -0400

    Readme updated

--- README.md
+++ README.md
@@ -1,3 +1,66 @@
+# Map Visualization of State Obesity Statistics
+
+## Overview
+
+This project utilizes React, Next.js, Tailwind CSS, and TypeScript to create an interactive map visualization displaying state obesity statistics. It integrates React-Map-GL for rendering maps, allowing users to explore obesity data in a visually engaging way. The application features the ability to search and filter data points on the map, enhancing user interactivity.
+
+## Technologies Used
+
+- **React**: A JavaScript library for building user interfaces.
+- **Next.js**: A React framework for building server-rendered applications.
+- **Tailwind CSS**: A utility-first CSS framework for rapid UI development.
+- **TypeScript**: A statically typed superset of JavaScript that adds optional types.
+
+**Why React.js, Next.js and TypeScript?**
+I choose to use React, Next.js, and TypeScript in my projects because of the unparalleled advantages they offer in terms of development speed, maintainability, and scalability. React's component-based architecture allows me to build modular and reusable user interfaces, making it easier to manage complex applications. Next.js, being a powerful React framework, simplifies server-side rendering, routing, and deployment, providing a seamless and optimized user experience. Additionally, TypeScript enhances my productivity by adding static types, enabling early error detection, and improving code readability, which leads to more reliable and maintainable code. The combination of React, Next.js, and TypeScript not only streamlines my development process but also ensures the creation of robust, high-quality web applications tailored to my project requirements.
+
+**Why Tailwind CSS?**
+I choose to use Tailwind CSS in my projects because of its high-efficient speed and simplicity. With Tailwind, I can rapidly develop stylish and responsive user interfaces without the hassle of writing extensive custom CSS. Its utility-first approach aligns perfectly with my workflow, allowing me to focus on functionality rather than spending time on intricate styling.
+
+## Accomplishments
+
+- Implemented an interactive map using React-Map-GL.
+- Integrated state obesity statistics in GeoJSON format.
+- Implemented search and filtering functionality for data points on the map.
+- Utilized Tailwind CSS for responsive and modern UI design.
+- Enhanced user experience with smooth map interactions.
+
+## Installation and Setup
+
+1. **Clone the repository:**
+
+   ```bash
+   git clone <repository-url>
+   cd map-visualization
+   ``` 
+2. **Install dependencies:**
+
+    ```bash
+    npm install
+    ```
+3. **Set up environment variables:**
+    Create a **'.env.local'** file in the project root and add your Mapbox access token:
+
+    ```bash
+    NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN=your-mapbox-access-token
+    ```
+    Replace **'your-mapbox-access-token'** with your actual Mapbox access token.
+4. **Run the application:**
+
+    ```bash
+    npm run dev
+    ``````
+    The application will be accessible at **'http://localhost:3000'**.
+
+## Usage
+ - **Exploring the Map:**
+    - Use the mouse or touch gestures to pan and zoom the map.
+    - Interact with the map to view different state obesity statistics.
+- **Search and Filter:**
+    - Use the search bar to find specific locations on the map.
+    - Apply filters to visualize specific obesity data points based on criteria.
+
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,42 +1,20 @@
 "use client"
 
-import { features } from 'process';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactMapGL,{Source, Layer, CircleLayer} from 'react-map-gl'
-import type {FeatureCollection} from 'geojson';
 
-import { Feature, Point } from 'geojson';
-
-interface GeoJsonProperties {
-  // Define your properties here
-  name: string;
-  // Add other properties as needed
-}
-
-const geojson: Feature<Point, GeoJsonProperties> = {
-  type: 'Feature',
-  geometry: {
-    type: 'Point',
-    coordinates: [-122.4, 37.8]
-  },
-  properties: {
-    name: 'Example Location', // Add your properties here
-    // Add other properties as needed
-  }
-};
 
 const layerStyle: CircleLayer = {
     id: 'point',
     type: 'circle',
     paint: {
-      'circle-radius': 10,
+      'circle-radius': 2,
       'circle-color': '#007cbf'
     }
   };
 
-  
-
 function Map(){
+
     const [viewState, setViewState] = useState({
         longitude: -100,
         latitude: 40,
@@ -55,71 +33,10 @@ function Map(){
         >
             <Source id='National_Obesity_By_State' 
                     type='geojson'  
-                    data={geojson}>
+                    data="https://raw.githubusercontent.com/uber/react-map-gl/master/examples/.data/us-income.geojson">
                         <Layer {...layerStyle}></Layer>
             </Source>
         </ReactMapGL>
 }
 
 export default Map
-// "use client"
-
-// import * as React from "react";
-// import mapboxgl from "mapbox-gl";
-// import "mapbox-gl/dist/mapbox-gl.css";
-// import { sources } from "next/dist/compiled/webpack/webpack";
-// // import the mapbox-gl styles so that the map is displayed correctly
-
-// function MapboxMap() {
-//     // this is where the map instance will be stored after initialization
-//     const [map, setMap] = React.useState<mapboxgl.Map>();
-
-//     // React ref to store a reference to the DOM node that will be used
-//     // as a required parameter `container` when initializing the mapbox-gl
-//     // will contain `null` by default
-//     const mapNode = React.useRef(null);
-
-//     React.useEffect(() => {
-//         const node = mapNode.current;
-//         // if the window object is not found, that means
-//         // the component is rendered on the server
-//         // or the dom node is not initialized, then return early
-//         if (typeof window === "undefined" || node === null) return;
-
-//         // otherwise, create a map instance
-//         const mapboxMap = new mapboxgl.Map({
-//             container: node,
-//             accessToken: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
-//             style: "mapbox://styles/mapbox/streets-v11",
-//             center: [-74.5, 40],
-//             zoom: 9,
-//         });
-
-//         mapboxMap.on("load", function(){
-//             console.log('yeahh')
-//             mapboxMap.addSource("National_Obesity_By_State",{
-//                 type:"geojson",
-//                 data:"../data/National_Obesity_By_State.geojson"});
-            
-//             mapboxMap.addLayer({
-//                 id:"National_Obesity",
-//                 type:"fill",
-//                 source:"National_Obesity_By_State"
-//             })
-//         })
-
-        
-//         // save the map object to React.useState
-//         setMap(mapboxMap);
-
-//         return () => {
-//             mapboxMap.remove();
-//         };
-//     }, []);
-
-//     return (<div className="h-1/2 h-screen">
-//         <div ref={mapNode} style={{ width: "100%", height: "100%" }}/>
-//     </div>);
-// }
-
-// export default MapboxMap
